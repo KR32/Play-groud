133ca6110a00768efd77d99bba1b823059b6f753
kalingaraj <kalingaraj34032@gmail.com>
2022-02-15 21:00:01 +0530

    assert keyword in python3

--- Python/core/exception_handling/try-except.py
+++ Python/core/exception_handling/try-except.py
@@ -9,3 +9,16 @@ try:
     number=number+1
 except:
     print('number')
+
+
+num = 6
+assert num != 6, "the given number is 6"
+print("the given number is not 6")
+
+
+# output:
+# number
+# Traceback (most recent call last):
+#   File "try-except.py", line 15, in <module>
+#     assert num != 6, "the given number is 6"
+# AssertionError: the given number is 6
